228009b9b00b0bcbee21ee8efd325ecb77b80112
palm214 <halabno225@gmail.com>
2026-07-08 17:49:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -163,6 +163,6 @@ setInterval(async () => {
 
 loadMassiveLibrary().then(() => {
     testCommentsFlow();
-    setInterval(testCommentsFlow, 90000);
+    setInterval(testCommentsFlow, 180000);
 });
 
